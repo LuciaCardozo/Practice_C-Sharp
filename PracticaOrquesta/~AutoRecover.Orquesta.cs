e3736ef71745eef57e8da4aad7fe7025ce4dc23b
lucia <lucia.t.cardozo@gmail.com>
2019-11-28 22:11:20 -0300

    ok

---
+++ PracticaOrquesta/~AutoRecover.Orquesta.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaOrquesta
+{
+    class Orquesta
+    {
+        public string nombre;
+        public string lugar;
+        public tipoOrquesta tipo;
+        public List<Musico> musicos;
+
+        public enum tipoOrquesta { Sinfonica, Camara, Filarmonica }
+
+        public Orquesta() { }
+
+        public Orquesta(string nombre, string lugar, tipoOrquesta orquesta)
+        {
+            this.nombre = nombre;
+            this.lugar = lugar;
+            this.tipo = orquesta;
+            this.musicos = new List<Musico>();
+        }
+        public string getOrquestaString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Nombre Orquesta: {0}, Lugar: {1}, Tipo: {2}\n", this.nombre, this.lugar, this.tipo);
+
+            return sb.ToString();
+        }
+
+        public static void imprimirOrquesta(Orquesta or)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(or.getOrquestaString());
+            if(or.musicos.Count>0)
+            {
+                sb.AppendFormat("Lista de Musico \n");
+                foreach(Musico element in or.musicos)
+                {
+                    sb.AppendFormat("Musico: {0} {1}\n", element.nombre,element.apellido);
+                    sb.AppendFormat("Instrumento: {0}\n", element.instrumento.nombre);
+
+                }
+            }
+            Console.WriteLine("{0}", sb.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PracticaOrquesta/~AutoRecover.Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaOrquesta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Orquesta or = new Orquesta("Orquesta1", "Avellaneda", Orquesta.tipoOrquesta.Sinfonica);
+            Console.WriteLine("Nombre: {0}, Lugar: {1}, Tipo: {2}", or.nombre, or.lugar, or.tipo);
+            Console.ReadLine();
+
+            Instrumento instrumento = new Instrumento("Guitarra", Instrumento.tipoInstrumento.Cuerdas);
+
+            Musico musicoPrincipal = new Musico("Carla", "Perez", 26, instrumento);
+            Console.WriteLine("Nombre: {0}, Apellido: {1}, Tipo: {2}, \nInstrumento {3}\n", 
+                musicoPrincipal.nombre, musicoPrincipal.apellido, musicoPrincipal.edad, 
+                musicoPrincipal.instrumento.getInstrumentoString());
+            Console.ReadLine();
+
+            Musico m2 = new Musico("Pepe", "Grillo", 23, instrumento);
+
+            or.musicos.Add(musicoPrincipal);
+            or.musicos.Add(m2);
+            Orquesta.imprimirOrquesta(or);
+
+            Orquesta or2 = new Orquesta("Orquesta2", "Quilmes", Orquesta.tipoOrquesta.Camara);
+        }
+    }
+}
