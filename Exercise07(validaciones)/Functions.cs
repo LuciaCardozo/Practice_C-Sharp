7550f0247ee46797c9f8738f94eb80c664c5d1a1
Lucia <jonathan.n.haedo@gmail.com>
2020-01-26 15:53:33 -0300

    validacionSoloNameDniCuit

---
+++ Exercise07(validaciones)/Functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Exercise07
+{
+    class Functions
+    {
+        public static bool IsValidName(string name)
+        {
+            Regex expression = new Regex(@"^[a-zA-Z]+$");
+            if (expression.IsMatch(name))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool IsValidNumber(string number)
+        {
+            Regex expression = new Regex(@"^[0-9]+$");
+            if (expression.IsMatch(number))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static bool IsValidDni(string dni)
+        {
+            Regex expression = new Regex(@"^[0-9]{8}$");
+            if (expression.IsMatch(dni))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        public static bool IsValidCuit(string cuit)
+        {
+            Regex expression = new Regex(@"^[0-9]{2}[-]{1}[0-9]{8}[-]{1}[0-9]{1}$");
+            if (expression.IsMatch(cuit))
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
---
+++ Exercise07(validaciones)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Exercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cuit;
+            string attempts;
+            do
+            {
+                Console.WriteLine("\n*Ingrese cuit con 2 (-) unicamente putx: ");
+                cuit = Console.ReadLine();
+                if (!Functions.IsValidCuit(cuit))
+                {
+                    Console.WriteLine("\n\tERROOOOOR");
+                }
+                else
+                {
+                    Console.WriteLine("\n\tCuit ok perri");
+                }
+                Console.WriteLine("\n*Desea reintentar? (si-no)");
+                attempts = Console.ReadLine();
+            } while (attempts != "no");
+            Console.WriteLine("\n\tGood bye!");
+            Console.ReadKey();
+        }
+    }
+}
